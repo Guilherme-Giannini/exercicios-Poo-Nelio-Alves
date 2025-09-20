674e5ef661f579e43742b744e5c7fab515c661ca
Guilherme Giannini <guilhermegianninimelo@gmail.com>
2025-09-20 10:06:03 -0300

    Operador de coalescência nula

---
+++ Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,19 @@
+﻿//Operador de coalescência nula
+
+namespace Exercicio02;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double? x = null;
+        double? y = 10;
+
+        double a = x ?? 5; // se x for null, a recebe 5
+        double b = y ?? 5; // se y for null, b recebe 5
+
+        Console.WriteLine(a); // 5
+        Console.WriteLine(b); // 10
+    }
+}
+
+
