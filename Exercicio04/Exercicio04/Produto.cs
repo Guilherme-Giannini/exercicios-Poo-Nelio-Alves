6273e01cf8036ad7170817c2df2857ae9769ee95
Guilherme Giannini <guilhermegianninimelo@gmail.com>
2025-09-20 10:47:45 -0300

    Vetores com objeto do tipo referência

---
+++ Exercicio04/Exercicio04/Produto.cs
@@ -0,0 +1,6 @@
+﻿namespace Exercicio04;
+class Produto
+{
+    public string Name { get; set; }
+    public double Preco { get; set; }
+}
---
+++ Exercicio04/Exercicio04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Exercicio04;
+
+  class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Produto[] vect = new Produto[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            string name = Console.ReadLine();
+            double preco = double.Parse(Console.ReadLine());
+            vect[i] = new Produto { Name = name, Preco = preco };
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += vect[i].Preco;
+        }
+
+        double avg = sum / n;
+        Console.WriteLine("AVERAGE PRICE = " + avg.ToString("F2"));
+    }
+}
