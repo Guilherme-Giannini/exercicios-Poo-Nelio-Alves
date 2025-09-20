63a5f3c1045104e7d82f22a8a57977e37fc9ac99
Guilherme Giannini <guilhermegianninimelo@gmail.com>
2025-09-20 11:13:02 -0300

    Exercício de fixação

---
+++ Exercicio05/Exercicio05/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercicio05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] rooms = new int[10];
+
+        int quantityRoomsRented = int.Parse(Console.ReadLine());
+        if (quantityRoomsRented > rooms.Length)
+        {
+            Console.WriteLine("Quantidade de quartos alugados excede o total disponível.");
+            return;
+        }
+
+        for (int i = 0; i < quantityRoomsRented; i++)
+        {
+            string[] rentedRoom = Console.ReadLine().Split(' ');
+            int roomNumber = int.Parse(rentedRoom[0]);
+            string name = rentedRoom[1];
+            string email = rentedRoom[2];
+            rooms[roomNumber] = 1;
+            Pessoas pessoa = new Pessoas();
+            pessoa.Name = name;
+            pessoa.Email = email;
+            pessoa.Room = roomNumber;
+            Console.WriteLine($"Aluguel: {pessoa.Room}, Nome: {pessoa.Name}, Email: {pessoa.Email}");
+        }
+    }
+
+}
