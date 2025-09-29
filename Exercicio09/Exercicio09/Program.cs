fd73d87d9a19a9f91ae9aa7119eef634d3695006
Guilherme Giannini <guilhermegianninimelo@gmail.com>
2025-09-29 07:38:36 -0300

    Revisão foreach

---
+++ Exercicio09/Exercicio09/Program.cs
@@ -0,0 +1,26 @@
+﻿// Laço for each 
+// isso é apenas uma revisão!
+
+using System.Runtime.Serialization.Formatters;
+
+namespace Exercicio09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] { "maria", "Alex", "Bob" };
+            for (int i = 0; i < vect.Length; i++)
+            {
+                Console.WriteLine(vect[i]);
+            }
+
+            Console.WriteLine( "´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+
+            foreach (string obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
