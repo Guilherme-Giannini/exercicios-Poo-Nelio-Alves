d5e8556f1f13c17c37c1148809d27d1b0fe7c248
Guilherme Giannini <guilhermegianninimelo@gmail.com>
2025-09-29 07:14:50 -0300

    Add files via upload

---
+++ exercicio06/exercicio06/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Course;
+
+    class Calculator
+    {
+    public static void Triple (ref int x)
+        {
+            x = x * 3;
+    }
+}
+
---
+++ exercicio06/exercicio06/Program.cs
@@ -0,0 +1,13 @@
+﻿// modficador de parametros ref e out
+
+namespace Course {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+        }
+    }
+}
