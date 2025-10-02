88791cd7ffa99ada3a3c336c639a511c31c08469
Guilherme Giannini <guilhermegianninimelo@gmail.com>
2025-10-02 08:55:07 -0300

    pt 1 - entities/enums/relacioamento de objetos

---
+++ Exercicio12/Exercicio12/Entities/HoursContract.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio12.Entities
+{
+    class HoursContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HoursContract()
+        {
+        }
+        public HoursContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+        public double TotalValue()
+        {
+            return ValuePerHour * Hours;
+        }
+    }
+}
---
+++ Exercicio12/Exercicio12/Entities/Worker.cs
@@ -0,0 +1,45 @@
+﻿using Exercicio12.Entities.Enums;
+using System.Collections.Generic;
+
+namespace Exercicio12.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HoursContract> Contracts { get; set; } = new List<HoursContract>();
+        public Worker()
+        {
+        }
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+        public void AddContract(HoursContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HoursContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HoursContract contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+            return sum;
+        }
+    }
+}
