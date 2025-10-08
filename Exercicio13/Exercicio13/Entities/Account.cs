30c0de0929b3078ac6ee1f8e4c3dc9cc691e9bc8
Guilherme Giannini <guilhermegianninimelo@gmail.com>
2025-10-08 08:18:48 -0300

    Herança

---
+++ Exercicio13/Exercicio13/Entities/Account.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio13.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string? Holder { get; set; }
+        public double Balance { get; set; }
+
+        public Account()
+        {
+        }
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+        public void withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+        public void deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
