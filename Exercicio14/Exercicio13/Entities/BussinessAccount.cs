2db27b5ecbda1e3ae8a8c6d703b038470930d374
Guilherme Giannini <guilhermegianninimelo@gmail.com>
2025-10-08 08:44:00 -0300

    Upcasting e Downcasting

---
+++ Exercicio14/Exercicio13/Entities/BussinessAccount.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio13.Entities
+{
+    internal class BussinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BussinessAccount()
+        {
+        }
+
+        public BussinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ Exercicio14/Exercicio13/Program.cs
@@ -0,0 +1,36 @@
+﻿using Exercicio13.Entities;
+
+namespace Exercicio13;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Account acc = new Account(1001, "Alex", 0.0);
+        BussinessAccount bacc = new BussinessAccount(1002, "Maria", 0.0, 500.0);
+
+        // UPCASTING
+        Account acc1 = bacc;
+        Account acc2 = new BussinessAccount(1003, "Bob", 0.0, 200.0);
+        Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+        // DOWNCASTING
+        BussinessAccount acc4 = (BussinessAccount)acc2;
+        acc4.Loan(100.0);
+
+        //BussinessAccount acc5 = (BussinessAccount)acc3; -> Erro de execusão, pois acc3 é do tipo savingsAccount
+        if (acc3 is BussinessAccount)
+        {
+            //BussinessAccount acc5 = (BussinessAccount)acc3;
+            BussinessAccount acc5 = acc3 as BussinessAccount;
+            acc5.Loan(200.0);
+            Console.WriteLine("Loan!");
+        }
+        else if (acc3 is SavingsAccount)
+        {
+            SavingsAccount acc5 = (SavingsAccount)acc3;
+            acc5.UpdateBalance();
+            Console.WriteLine("Update!");
+        }
+    }
+}
